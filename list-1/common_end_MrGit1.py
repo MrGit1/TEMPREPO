86f365d79e170345e25ec2205d7c31bce977ceab
MrGit1 <1valjivam@hdsb.ca>
2020-09-28 12:55:31 -0400

    TEMPREPO

---
+++ list-1/common_end_MrGit1.py
@@ -0,0 +1,54 @@
+'''
+CodingBat Python Activity "common_end" from list-1.
+codingbat.com
+'''
+
+failures = 0
+
+def commonEnd(nums1, nums2):
+    '''Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+
+    common_end([1, 2, 3], [7, 3]) → True
+    common_end([1, 2, 3], [7, 3, 2]) → False
+    common_end([1, 2, 3], [1, 3]) → True
+    '''
+    pass
+# Given 2 arrays of ints, a and b, return True if they have the same first element
+# or they have the same last element. Both arrays will be length 1 or more.
+#def commonEnd(a,b):
+#    if (commonEnd[2] == commonEnd[-1]) or (commonEnd[0] == commonEnd[3]):
+#        pass
+#
+
+
+#COMMON END AND MONKEY
+
+
+
+def catchFailures(testCase, result):
+    global failures
+
+    try:
+        assert eval(testCase) == result, "Test Case Failed: "+testCase+ " → "+str(result)
+    except Exception as e:
+        failures += 1
+        print(e)
+
+
+
+def test():
+    global failures
+    catchFailures('commonEnd([1, 2, 3], [7, 3])', True)
+    catchFailures('commonEnd([1, 2, 3], [7, 3, 2])', False)
+    catchFailures('commonEnd([1, 2, 3], [1, 3])', True)
+    catchFailures('commonEnd([1, 2, 3], [1])', True)
+    catchFailures('commonEnd([1, 2, 3], [2])', False)
+
+    if failures > 0:
+        print("\r\n "+ str(failures) + " failed test case(s)   :(")
+    else:
+        print("Good job -- problem solved!")
+
+
+test()
--- warmup-1/monkey_trouble_MrGit1.py
+++ warmup-1/monkey_trouble_MrGit1.py
@@ -5,8 +5,7 @@ codingbat.com
 
 failures = 0
 
-def monkey_trouble(a_smile, b_smile):
-    '''
+'''
 
 We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
@@ -15,26 +14,30 @@ monkey_trouble(True, True) → True
 monkey_trouble(False, False) → True
 monkey_trouble(True, False) → False
 '''
-    pass
 
+def monkey_trouble(a_smile, b_smile):
+    if (a_smile == True) and (b_smile == True):
+        return True
+    if (a_smile == False) and (b_smile == False):
+        return True
 
-
-
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
+# We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
 def catchFailures(testCase, result):
     global failures
-    
+
     try:
         assert eval(testCase) == result, "Test Case Failed: "+testCase+ " → "+str(result)
     except Exception as e:
         failures += 1
         print(e)
-            
+
 
 
 def test():
     global failures
-    
+
     catchFailures('monkey_trouble(True, True)', True)
     catchFailures('monkey_trouble(False, False)', True)
     catchFailures('monkey_trouble(True, False)', False)
