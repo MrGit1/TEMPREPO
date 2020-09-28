e6b94f8fd1fdeea97ad1bb34ad554cb83033ec8b
MrGit1 <1valjivam@hdsb.ca>
2020-09-28 12:59:57 -0400

    Update monkey_trouble_MrGit1.py

--- warmup-1/monkey_trouble_MrGit1.py
+++ warmup-1/monkey_trouble_MrGit1.py
@@ -20,6 +20,7 @@ def monkey_trouble(a_smile, b_smile):
         return True
     if (a_smile == False) and (b_smile == False):
         return True
+    return False
 
 # We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
 # We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
